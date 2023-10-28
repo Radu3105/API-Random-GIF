bd64fdd1758bedc6db7f81b1e74d8affc72b0159
Radu3105 <radu.girlea@yahoo.com>
2023-10-28 12:54:17 +0300

    Use async/await function w/try/catch block instead of promises for getting results

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 const API_KEY = 'TW8IRNPbb5h7mc4WKaRhcEnt5UIlekPO';
-// const API_ENDPOINT = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchQuery}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
-
 const app = document.querySelector("#app");
 const search = document.querySelector('#search');
 const grid = document.querySelector('.grid');
@@ -11,27 +9,29 @@ function removeAllImages() {
     }
 }
 
-search.addEventListener('search', () => {
-    console.log("Searching for " + search.value);
-    removeAllImages();
-    const searchQuery = search.value;
-    fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchQuery}&limit=9&offset=0&rating=g&lang=en&bundle=messaging_non_clips`, { mode: "cors" })
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (response) {
-            response.data.forEach((gif) => {
-                const gridItem = document.createElement('div');
-                const img = document.createElement("img");
-                img.src = gif.images.original.url;
-                gridItem.classList.add('grid-item');
-                gridItem.append(img);
-                grid.appendChild(gridItem);
-            });
-        })
-        .catch(function (error) {
-            console.error('Error:', error);
+async function getResult() {
+    try {
+        removeAllImages();
+        const searchQuery = search.value;
+        let response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchQuery}&limit=9&offset=0&rating=g&lang=en&bundle=messaging_non_clips`,
+                            { 
+                                mode: "cors",
+                            }
+                        );
+        let data = await response.json();
+        // console.log(data);
+        data.data.forEach((gif) => {
+            const gridItem = document.createElement('div');
+            const img = document.createElement("img");
+            img.src = gif.images.original.url;
+            gridItem.classList.add('grid-item');
+            gridItem.append(img);
+            grid.appendChild(gridItem);
         });
-});
+    }
+    catch(error) {
+        console.error('Error: ', error.message);
+    }
+}
 
-
+search.addEventListener('search', getResult);
